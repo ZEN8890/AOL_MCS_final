36a4cb08f6e2f357910293ddd24605dc23c7a635
ZEN8890 <Steven.gunawan8@hotmail.com>
2024-06-18 23:51:37 +0700

    puff and poof final mcs

---
+++ app/src/main/java/com/example/puffandpoof/model/Doll.kt
@@ -0,0 +1,17 @@
+package com.example.puffandpoof.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "doll")
+data class Doll(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val desc: String,
+    val name: String,
+    val size: String,
+    val price: String,
+    val rating: String,
+    val imageLink: String
+)
+
